38b6a1238c72ca9787b437dabf6a489c24f42c97
Juan Carlos Medina <juanca@mavenlink.com>
2018-11-19 11:19:42 -0800

    Convert configurable paths into CLI options

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 const hypernova = require('hypernova/server');
 const path = require('path');
-const railsRoot = require('../config/helpers/rails-root.js');
 const winston = require('winston');
 
 const production = process.env.NODE_ENV === 'production';
-const statsPath = '../public/compiled/ssr/manifest.json';
+
+const defaultOptions = {
+  'assets-path': __dirname,
+  'logs-path': path.join(__dirname, 'hypernova.log'),
+  'manifest-path': __dirname,
+};
+
+const options = process.argv
+  .slice(2)
+  .reduce((options, arg) => {
+    if (arg.indexOf('=')) {
+      const [key, value] = arg.split('=');
+      options[key.slice(2)] = value;
+    }
+
+    return options;
+  }, defaultOptions);
 
 hypernova({
   devMode: !production,
 
   getComponent(name) {
-    const stats = require(statsPath); // eslint-disable-line global-require, import/no-dynamic-require
+    const stats = require(options['manifest-path']); // eslint-disable-line global-require, import/no-dynamic-require
     if (!production) delete require.cache[require.resolve(statsPath)];
 
     const entryPath = path.join(
-      path.join(railsRoot, 'public', 'compiled', 'ssr'),
+      options['assets-path'],
       stats.assetsByChunkName[name].find(file => file.includes('.js')),
     );
 
@@ -24,7 +39,7 @@ hypernova({
   loggerInstance: winston.createLogger({
     transports: [
       production
-        ? new winston.transports.File({ filename: 'log/hypernova.log' })
+        ? new winston.transports.File({ filename: options['logs-path'] })
         : new winston.transports.Console(),
     ],
   }),
