24aa6de9e1e28424262495f0846f01727a3bc2e0
Juan Carlos Medina <juanca@mavenlink.com>
2018-11-20 11:47:27 -0800

    Looks like the login component throws an error when it is missing the errors property
    This should actually be defaulted to something. I also noticed that we are logging into the console when there are prop-type errors. This is most likely the Winston configuration. I have not expose the configuration as an API but this seems like a good use-case for leverage external logging infrastructure (e.g. New Relic).

--- test-curl.sh
+++ test-curl.sh
@@ -1,3 +1,5 @@
 curl -X POST localhost:13337/batch \
   -H Content-Type:application/json \
-  -d '{"foo": {"name":"login_page", "data": {}}}'
+  -d '{"foo": {"name":"login_page", "data": {
+    "errors":""
+  }}}'
